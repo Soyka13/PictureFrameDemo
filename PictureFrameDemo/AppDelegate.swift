fec8c36ad4a9d692a5bfdaa06bd262d87b9c890b
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2021-05-24 10:54:26 +0300

    Initial commit

---
+++ PictureFrameDemo/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  PictureFrameDemo
+//
+//  Created by Olena Stepaniuk on 24.05.2021.
+//
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
